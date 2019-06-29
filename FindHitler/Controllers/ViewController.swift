be523acbeaa854f09648b074ebe563e426971713
John Choi <johnchoi1003@icloud.com>
2019-06-28 22:56:33 -0400

    Basic functions implemented

--- FindHitler/Controllers/ViewController.swift
+++ FindHitler/Controllers/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 John Choi. All rights reserved.
 //
 
+import Foundation
 import UIKit
 import WebKit
 
@@ -23,34 +24,50 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = URLRequest(url: urlAddress!)
         webkit?.load(url)
         webkit.navigationDelegate = self
+        webkit.allowsBackForwardNavigationGestures = true // for debugging only
         
         scoreLbl.text = String(format: "Score: %d", score)
     }
     
+    @IBAction func resetBtnActivated(_ sender: UIButton) {
+        let urlAddress = URL(string: "https://en.wikipedia.org/wiki/Special:Random") // start point
+        let url = URLRequest(url: urlAddress!)
+        webkit?.load(url)
+        webkit.navigationDelegate = self
+        webkit.allowsBackForwardNavigationGestures = true // for debugging only
+        
+        webkit.isUserInteractionEnabled = true
+        score = 0
+        scoreLbl.text = String(format: "Score: %d", score)
+    }
+    
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        print(webView.url!.absoluteString)
-        if webView.url!.absoluteString == "https://en.wikipedia.org/wiki/Adolf_Hitler" {
-            let alert = UIAlertController(title: "Congratulations!", message: "You found the Fuhrer :/", preferredStyle: .alert)
+        print("Clicked link " + navigationAction.request.url!.absoluteString)
+        let currentUrl = navigationAction.request.url!.absoluteString
+        // check if the loading page is the target page
+        if currentUrl == "https://en.m.wikipedia.org/wiki/Adolf_Hitler" {
+            // if target page, display a score dialog
+            let alert = UIAlertController(title: "Congratulations!", message: String(format: "You won with a score of %d!", score), preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
             
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            
-            self.present(alert, animated: true)
-            webkit.isUserInteractionEnabled = true
+            decisionHandler(.allow)
+            webkit.isUserInteractionEnabled = false
             return
         }
+        
+        // if not the target page
+        // if the clicked link is hyperlink
         if navigationAction.navigationType == WKNavigationType.linkActivated {
             print("link")
             score += 1
             
             scoreLbl.text = String(format: "Score: %d", score)
             decisionHandler(WKNavigationActionPolicy.allow)
-            
-            print("After" + webView.url!.absoluteString)
-            return
+        } else {
+            print("no link")
+            decisionHandler(WKNavigationActionPolicy.allow)
         }
-        print("no link")
-        decisionHandler(WKNavigationActionPolicy.allow)
-        print("After" + webView.url!.absoluteString)
     }
 }
 
